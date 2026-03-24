475fe6045021c66154facc75843c6fc213b2e50e
gajdascz <gajdascz@gmail.com>
2026-03-24 03:36:13 -0400

    fix: ci
    add ignore version packages to commitlint

--- packages/configs/src/runtime/commitlint/core/types.ts
+++ packages/configs/src/runtime/commitlint/core/types.ts
@@ -4,6 +4,7 @@ import type { RequiredProps } from '@toolbox-ts/types/defs/object';
 export interface InputConfig extends UserConfig {
   scopes?: string[];
   rules?: Partial<Omit<RulesConfig, 'scope-enum'>>;
+  usingChangesets?: boolean;
 }
 export type Defaults = RequiredProps<InputConfig, 'extends' | 'rules' | 'defaultIgnores'> & {
   extends: string[];
